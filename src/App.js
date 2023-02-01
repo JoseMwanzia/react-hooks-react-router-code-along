c93f2bb21f07ad118d67b04dc036fe01e0be066e
joseph mwanzia <makamwanzia@gmail.com>
2023-02-01 22:01:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/about" element={<About />}>
+
+        </Route>
+        <Route path="/login" element={<Login />}>
+
+        </Route>
+        <Route exact path="/" element={<Home />}>
+
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
